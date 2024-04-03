71881e06d74351e76f1222b957e4977e0a1a4f92
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-04-03 05:16:57 +0100

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,13 +3,16 @@ package { 'nginx':
   ensure => installed,
 }
 
+# Get the hostname of the server
+$hostname = inline_epp('<%=  @facts["hostname"] %>')
+
 # Configure the custom HTTP header
 file { '/etc/nginx/sites-available/default':
   content => "server {
     listen 80 default_server;
     listen [::]:80 default_server;
     server_name _;
-    add_header X-Served-By \$hostname;
+    add_header X-Served-By ${hostname};
     location / {
       root /var/www/html;
       index index.html index.htm;
