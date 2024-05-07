8813abed63049693f5bae65c09946ac896c5e4cb
truthtek <adedokunadebayolukman@gmail.com>
2024-05-07 12:42:14 -0700

    commit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,28 +1,18 @@
 #!/usr/bin/python3
-import requests
+"""
+queries the Reddit API and prints the titles of the first
+10 hot posts listed for a given subreddit
+"""
+from requests import get
+
 
 def top_ten(subreddit):
-    """
-    Queries the Reddit API and prints the titles of the first 10 hot posts
-    listed for a given subreddit.
-    If the subreddit is invalid, print None.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {"User-Agent": "python:reddit-api-project:v1.0 (by /u/your_username)"}
-
+    """prints titles of first 10 hot posts listed for a given subreddit"""
+    r_subreddit = get('http://www.reddit.com/r/{}/hot.json'.
+                      format(subreddit), headers={'User-Agent': 'Mybot'})
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        response.raise_for_status()
-        data = response.json()
-        posts = data["data"]["children"]
-
-        if not posts:
-            print("None")
-            return
-
-        for post in posts:
-            title = post["data"]["title"]
-            print(title)
-
-    except (requests.exceptions.HTTPError, KeyError):
-        print("None")
+        subreddit_dict = r_subreddit.json()
+        for i in range(10):
+            print(subreddit_dict['data']['children'][i]['data']['title'])
+    except:
+        print(None)
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,42 +1,31 @@
-
 #!/usr/bin/python3
-import requests
+"""
+a recursive function that queries the Reddit API and returns a list containing
+the titles of all hot articles for a given subreddit. If no results are found
+for the given subreddit, return None
+"""
+from requests import get
+
 
 def recurse(subreddit, hot_list=[], after=None):
-    """
-    Returns a list of titles of all hot posts for a given subreddit.
-    If no results are found for the given subreddit, the function returns None.
-
-    Args:
-        subreddit (str): The subreddit to search for hot posts.
-        hot_list (list, optional): The list to store hot post titles.
-        after (str, optional): The `after` parameter for the Reddit API.
-
-    Returns:
-        list or None: A list of hot post titles or None if the subreddit is invalid.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {"User-Agent": "python:reddit-api-project:v1.0 (by /u/your_username)"}
-    params = {"limit": 100}
-
-    if after:
-        params["after"] = after
-
+    """returns a list containing the titles of all hot articles
+    for a given subreddit"""
     try:
-        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-        response.raise_for_status()
-        data = response.json()
-        posts = data["data"]["children"]
+        r = get('https://www.reddit.com/r/{}/hot.json?limit=100&&'
+                'after={}'.format(subreddit, after),
+                headers={'User-Agent': 'bc'})
+        subreddit_dict = r.json()
 
-        if not posts:
+        for i in range(len(subreddit_dict['data']['children'])):
+            hot_list.append(subreddit_dict['data']['children'][i]
+                            ['data']['title'])
+
+        after = subreddit_dict['data']['after']
+
+        if after is None:
             return hot_list
 
-        for post in posts:
-            title = post["data"]["title"]
-            hot_list.append(title)
-
-        after = data["data"]["after"]
         return recurse(subreddit, hot_list, after)
 
-    except (requests.exceptions.HTTPError, KeyError):
+    except:
         return None
