cdd700ebabc1d454d0e452e1b09b232c7e587825
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-05-16 08:46:48 +0100

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,11 @@
-# Fix nginx error 24: too many open files
-
-exec {'fix'
-    provider => shell,
-    command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
-    before   => Exec['restart nginx']
+file { '/etc/nginx/nginx.conf':
+  ensure  => file,
+  content => template('nginx/nginx.conf.erb'),
+  notify  => Service['nginx'],
 }
 
-exec {'restart nginx':
-    provider => shell,
-    command  => 'sudo service nginx restart'
+service { 'nginx':
+  ensure => running,
+  enable => true,
 }
+
