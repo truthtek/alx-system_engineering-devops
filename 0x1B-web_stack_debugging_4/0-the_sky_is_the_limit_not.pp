e7fe564bb063667ffb4aa780ef98c7ba095348e8
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-05-16 08:50:32 +0100

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,46 +1,11 @@
-file { '/etc/nginx/nginx.conf':
-  ensure  => file,
-  content => template('nginx/nginx.conf.erb'),
-  notify  => Service['nginx'],
+# fix max open file limit error
+exec {'increase max open files limit':
+  command => 'sed -i "s|15|15000|g" /etc/default/nginx',
+  path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/'
 }
 
-service { 'nginx':
-  ensure => running,
-  enable => true,
+exec {'restart nginx':
+  require => Exec['increase max open files limit'],
+  command => 'sudo service nginx restart',
+  path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/'
 }
-
-worker_processes auto;
-events {
-  worker_connections 1024;
-}
-http {
-    sendfile on;
-    tcp_nopush on;
-    tcp_nodelay on;
-    keepalive_timeout 65;
-    types_hash_max_size 2048;
-
-    include /etc/nginx/mime.types;
-    default_type application/octet-stream;
-
-    # Buffers
-    client_body_buffer_size 10K;
-    client_header_buffer_size 1k;
-    large_client_header_buffers 4 32k;
-
-    # Timeouts
-    client_body_timeout 12;
-    client_header_timeout 12;
-    send_timeout 10;
-}
-http {
-    gzip on;
-    gzip_disable "msie6";
-    gzip_vary on;
-    gzip_proxied any;
-    gzip_comp_level 6;
-    gzip_buffers 16 8k;
-    gzip_http_version 1.1;
-    gzip_types text/plain text/css application/json application/javascript text/xml application/xml application/xml+rss text/javascript;
-}
-
